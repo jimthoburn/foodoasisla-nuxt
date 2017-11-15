122b357585b8b84d6cf1e7f195d45c87f6923b2b
Jim Thoburn <jim@jimthoburn.com>
2017-11-15 10:35:26 -0800

    Simplify code

--- pages/index.vue
+++ pages/index.vue
@@ -9,10 +9,25 @@
 <script>
 import Locations from '~/components/Locations.vue'
 
-import getLocationsData from '~/util/getLocationsData.js'
+import findUserLocation from '~/util/findUserLocation.js'
+import getLocationFromPageURI from '~/util/getLocationFromPageURI.js'
+import getSortedLimitedLocations from '~/util/getSortedLimitedLocations.js'
+
+import locations from '~/data/locations.js'
 
 export default {
   components: { Locations },
-  asyncData: getLocationsData
+  asyncData: function ({route, app}, callback) {
+    findUserLocation({route, app, locations})
+      .then(function (youAreHere) {
+        let sortedLocations = getSortedLimitedLocations({route, youAreHere})
+        let selectedLocation = getLocationFromPageURI({route, locations})
+        callback(null, {
+          locations: sortedLocations,
+          youAreHere: youAreHere,
+          selectedLocation: selectedLocation
+        })
+      })
+  }
 }
 </script>
