9cfa7c4242891a6e4cc850bba2cbd53570a71020
Jim Thoburn <jim@jimthoburn.com>
2021-02-15 17:49:44 -0800

    Hide this site from search engines

--- nuxt.config.js
+++ nuxt.config.js
@@ -5,6 +5,9 @@ module.exports = {
   head: {
     title: 'Food Oasis Los Angeles',
     meta: [
+      // Since this is just an experiment
+      { name: 'robots', content: 'noindex' },
+
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       // { hid: 'description', name: 'description', content: 'Nuxt.js project' }
